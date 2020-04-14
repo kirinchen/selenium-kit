3d72f76b2b2b869b07594efed210e6e47854a452
kirin <kirin.chen1001@gmail.com>
2020-04-14 22:55:33 +0800

    Fix Chrome

--- app.ts
+++ app.ts
@@ -1,1 +1,11 @@
+import { SeleniumKit } from "./tool/SeleniumKit";
+
 console.log('Hello world');
+
+
+let a = async () => {
+    let sk = new SeleniumKit();
+    await sk.init();
+    await sk.driver.get('https://store.line.me/stickershop/product/1149795/?ref=Desktop');
+
+};
